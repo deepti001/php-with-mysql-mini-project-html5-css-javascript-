5b9c4aaaca13f595e3e4645067afdc5fd12f8f0b
deepti001 <er.deepti2013@gmail.com>
2018-05-11 14:49:16 +0530

    Final_Changes
    made a change to show the details of the student. and added the javascript part to hide it until someone don't click on the submit button

--- index.php
+++ index.php
@@ -4,56 +4,121 @@
 		<title> Student Management System	</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="css/style.css" type="text/css">
-<style>
-select{
-background-color:#4B97D3;
-color:#FD2907;
-box-shadow:inset 0 0 10px 0 rgba(0,0,0,0.6);
-margin-top: 8px;
-padding:0.3% 0.4%;
-border-radius: 5px;
-width: 10%;
-border: 2px solid blue;
-margin-left:2%;
-}
-
-#roll{
-	display: inline !important;
-}	
-
+<style>	
 </style>
 	</head>
 	<body>
-		<nav><a href="login.php">Admin Login</a></nav>		
+		<!--Navigation to the admin Login Page-->
+		<nav class="adminLoginIcon"><a href="login.php" class="adminLink"><span class="backgroundLoginLink">Admin Login</span></a></nav>		
+		
+		<!--Main Heading-->
 		<h1> Welcome to Student Management System	</h1>
+
+		<!--Form to enter the standard and RollNumber of student to visit his/her corresponding Details -->
+		<form class="infoForm" action="index.php" method="post">
+			<h2 class="imageContainer">
+				STUDENT INFORMATION
+			</h2>
 		
-		<form action="index.php" method="post">
-			<div class="imageContainer">
-				Student Information
-			</div>
-		
+			<!--A dropdown menu bar to enter the standard of the student-->
 			<div class="container">
-				<label for="std"> Choose Standard:	</label>
-				<select class="standard" name="std" required>
-					<option>	Class 6th	</option>
-					<option>	Class 7th	</option>
-					<option>	Class 8th	</option>
-					<option>	Class 9th	</option>
-					<option>	Class 10th	</option>
+				<label for="STD"> Choose Standard:	</label>
+				<select class="standard"  id="STD" name="std" required>
+					<option value="9">	Class 9th	</option>
+					<option value="10">	Class 10th	</option>
+					<option value="11">	Class 11th	</option>
+					<option value="12">	Class 12th	</option>
 				</select>
 				<br>
 			</div>
 			
+			<!--A textbox to enter the rollno. of the student-->
 			<div class="container">
-			<label for="rollno"> Enter RollNo	</label>
-				<input type="text" id="roll" placeholder="Enter Roll No." name="rollno" required style="display:inline">
+			<label for="roll"> Enter RollNo:	</label>
+				<input type="text" class="rollno" id="roll" placeholder="Enter Roll No." name="rollno" required style="display:inline">
 			</div>
 
 			<div class="container">		
-					<button type="submit"> Login </button>
+					<button type="submit" name="submit" > Show Information </button>
 			</div>
-		</form>		
+		</form>
 		
+		<!--The table which shows the detail of the student-->
+		<div id="studentDetail">
+		<table>
+		    <thead>	
+			<tr>
+				<th> S.No. </th>
+				<th> Standard	</th>
+				<th> RollNo.	</th>
+				<th> Student Name	</th>
+				<th> City </th>
+				<th> Parent's Contact No.	</th>
+				<th> Photo	</th>
+			</tr>
+		    </thead>
+		    <tbody>
+			    <!--if button is clicked then fetch the information from the database and displays it on the mainpage itself-->
+			<?php 
+				if(isset($_POST["submit"]))
+				{
+					include("dbcon.php");
+			
+					$std = $_POST["std"];
+					$rollno = $_POST["rollno"];
+				
+					$query = "SELECT * FROM student where standard='$std' AND rollno='$rollno' ";
+			
+					$run = mysqli_query($con, $query);
+			
+					if(mysqli_num_rows($run)<1)
+					{
+						echo "<tr><td colspan='7' align='center'>No Records Found</td></tr>";		
+					}		
+					else
+					{	
+						$count = 1;
+						while($data=mysqli_fetch_assoc($run))
+						{	
+							?>
+							<tr>
+								<td> <?php echo $count; ?>					</td>
+								<td> <?php echo $data['standard']; ?>	</td>
+								<td> <?php echo $data['rollno']; ?>		</td>
+								<td> <?php echo $data['name']; ?>		</td>
+								<td> <?php echo $data['city']; ?>	 	</td>
+								<td> <?php echo $data['pcont']; ?>	 	</td>
+								<td> <img src="dataimg/<?php echo $data['image']?>" >	 </td>
+							</tr>
+							<?php	
+							$count++;		
+						}	
+					}
+				}
+			?>
+			    </tbody>
+			</table>
+		</div>
 		
+		<!--Initially To make the student details table hidden-->	
+		<?php
+		
+		 if(isset($_POST["submit"]))
+		 {			 
+		 }
+		 else
+		 {
+			?>
+			<script>
+				function hideFunc(){
+				var x = document.getElementById("studentDetail");
+				x.style.visibility="hidden";	
+				}
+				hideFunc();
+			</script>
+			<?php		 
+		 }
+		?>
+	
 	</body>
 </html>
