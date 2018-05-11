b42a3047d9d57350b0a8c04d420f405bd07ce774
deepti001 <er.deepti2013@gmail.com>
2018-05-11 14:55:14 +0530

    comments and final changes

--- login.php
+++ login.php
@@ -1,25 +1,35 @@
+<?php
+	session_start();
+ 	if(isset($_SESSION["userID"]))
+ 	{
+		 	header("location:admin/admindash.php");
+ 	}	
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
-		<title> Login Illustration	</title>
+		<title> Admin Login	</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="css/style.css" type="text/css">
-		
 	</head>
-	<body>
+
+	<body class="body">
 		
-		<form action="abc.php">
+		<h1> Admin Login	</h1>
+		<!--Login Form of Admin-->
+		<form class="loginForm" action="login.php" method="post">
 			<div class="imageContainer">
 				<img src="images/loginImage.png" alt="avatar" class="avatar">
 			</div>
-		
+
 			<div class="container">
-				<label for="uname"> UserName	</label>
-				<input type="text" placeholder="Enter UserName" name="uname" required> <br>
-				<label for="pwd"> Password	</label>
-				<input type="password" placeholder="Enter Password" name="pwd" required>
+				<label for="UN"> UserName	</label>
+				<input class="uname" type="text" placeholder="Enter UserName" name="uname" id="UN" required> <br>
+				<label for="PWD"> Password	</label>
+				<input type="password" id="PWD" placeholder="Enter Password" name="pwd" required>
 			</div>
-			
+
 			<div class="container">
 				<label> 
 					<input type="checkbox" name="remember" checked> Remember Me </input>
@@ -28,9 +38,41 @@
 			</div>
 
 			<div class="container">
-				
-								<button type="submit"> Login </button>
+				<button type="submit" name="login"> Login </button>
 			</div>
 		</form>
 	</body>
 </html>
+
+<?php
+
+include("dbcon.php");
+
+//check if login button is clicked with corresponding information , and checks whether the details exist in the database , if exist so starts the corresponding access and provide the session id
+if(isset($_POST['login']))
+{
+	$username = $_POST["uname"];
+	$pwd = $_POST["pwd"];
+	
+	$query = "SELECT * FROM admin WHERE username='$username' AND password='$pwd' ";
+	$run = mysqli_query($con, $query);
+	
+	$row = mysqli_num_rows($run);
+	if($row<1)
+	{
+		?>		
+		<script>
+			alert("Username or Password not matched");
+			window.open("login.php","_self");
+		</script>
+		<?php	
+	}
+	else
+	{
+		$data = mysqli_fetch_assoc($run);
+		$id = $data["id"];
+		$_SESSION["userID"] = $id;
+		header("location:admin/admindash.php");
+	}
+}
+?>
